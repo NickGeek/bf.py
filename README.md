eb2f1e95acfddbbda353c7ad67b006bec517a346
Nick Webster <nick@nick.geek.nz>
2014-02-13 21:08:40 +1300

    Everything except loops and reading from a file

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 bf.py
 =====
 
-A tiny BF interpreter written in Python 3 that supports both reading from files and an interactive shell.
+A tiny [BF](http://esolangs.org/wiki/Brainfuck) interpreter written in Python 3 that supports both reading from files and an interactive shell.
+
+###Usage:
+####Running from a file:
+```
+python3 bf.py filename.ext
+```
+####Running the interactive shell:
+```
+python3 bf.py
+```
---
+++ bf.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+#TODO:
+#[]
+#Reading from a file
+
+from sys import argv as arg
+
+#Setup the memory
+memory = [0]
+cell = 0
+
+#The interpreter
+def interpret(code):
+	#Setup vars
+	global memory
+	global cell
+	output = ""
+
+	#Sort the code to be processed character by character
+	code = list(code)
+
+	for c in code:
+		if c == "+":
+			#Increment the cell
+			memory[cell] += 1
+		elif c == "-":
+			#Decrement the cell
+			if memory[cell] > 0:
+				memory[cell] -= 1
+		elif c == ">":
+			#Move to the next cell/create it
+			cell += 1
+			if len(memory)-1 < cell:
+				memory.append(0)
+		elif c == "<":
+			if cell > 0:
+				cell -= 1
+			else:
+				print("Error: Data pointer out of range.")
+		elif c == '.':
+			#Print value of cell
+			output += chr(memory[cell])
+		elif c == ',':
+			#Add the value of a string to the cell
+			memory[cell] = ord(str(input(".. ")))
+		elif c == "[":
+			pass
+		elif c == "]":
+			pass
+
+
+	#Return the memory
+	return str(output)+"\n=> {"+str(memory[cell])+"} "+str(memory)
+
+#The interactive shell
+def interactive():
+	print("bf.py 1.0.0\nhttp://github.com/NickGeek/bf.py")
+
+	line = True
+	while line:
+		line = str(input("> "))
+		if line:
+			print(interpret(line))
+
+try:
+	filename = arg[1]
+except:
+	interactive()
